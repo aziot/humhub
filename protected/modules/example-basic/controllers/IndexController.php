cbe4c1f9c5c5d1bbea01f667f8bad16c7789bc31
aziot <ayis.ziotopoulos@utexas.edu>
2021-03-05 18:58:05 +0100

    Refactor the index controller. Add the calling user to the newly created group

--- protected/modules/example-basic/controllers/IndexController.php
+++ protected/modules/example-basic/controllers/IndexController.php
@@ -9,6 +9,41 @@ use humhub\components\Controller;
 
 class IndexController extends Controller
 {
+	
+	const HUMHUB_SERVICE_URL = 'https://www.ziotopoulos.space/api/v1/space';
+	
+	/**
+	 * Sets the options for a curl object to be used for querying the API.
+	 *
+	 * @return boolean
+	 */
+	public function setOptionsForCurlObject($url, &$ch)
+	{
+		if (curl_setopt_array($ch, array(CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_HTTPAUTH => CURLAUTH_BASIC)) ) {
+			return true;
+		}
+
+		return false;
+	}
+	
+	/**
+	 * Form the URL to query for the Knowledge Graph.
+	 *
+	 * @return string
+	 */
+	public function getUrlToQueryKG($title)
+	{
+		require '/var/www/humhub/protected/modules/example-basic/controllers/.api_key';
+		$params = array(
+			'query' => $title, 
+			'limit' => 10, 
+			'indent' => TRUE, 
+			'key' => $api_key);
+		return self::HUMHUB_SERVICE_URL . '?' . http_build_query($params);
+	}
+	
 	/**
 	 * Query the knowledge graph for information on this book.
 	 * Return true if the query is successful, else false.
@@ -17,17 +52,11 @@ class IndexController extends Controller
 	 */
 	public function queryKG(&$model)
 	{
-		require '/var/www/humhub/protected/modules/example-basic/controllers/.api_key';
-		$service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
-		$params = array(
-			'query' => $model->title, 
-			'limit' => 10, 
-			'indent' => TRUE, 
-			'key' => $api_key);
-		$url = $service_url . '?' . http_build_query($params);
+		$url = $this->getUrlToQueryKG($model->title);
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		if (!$this->setOptionsForCurlObject($url, $ch)) {
+			return false;
+		}
 		$response = json_decode(curl_exec($ch), true);
 		curl_close($ch);
 
@@ -48,30 +77,50 @@ class IndexController extends Controller
 		}
 		return false;
 	}
+	
+	/**
+	 * Call the HUMHUB REST API.
+	 *
+	 * @return 
+	 */
+	public function callRestApi($data, &$ch)
+	{
+		if (!$this->setOptionsForCurlObject(self::HUMHUB_SERVICE_URL, $ch)) {
+			return;
+		}
+		require '/var/www/humhub/protected/modules/example-basic/controllers/.rest_api'; 
+		curl_setopt($ch, CURLOPT_USERPWD, "admin:$rest_api");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		$response = json_decode(curl_exec($ch), true);
+		curl_close($ch);
+	}
+	
+	/**
+	 * Adds a user to a space.
+	 *
+	 * @return
+	 */
+	public function addUserToSpace($space_id, $user_id, &$ch)
+	{
+		$data = array(
+			'id' => $space_id,
+			'userId' => $user_id);
+		$this->callRestApi($data, $ch);
+	}
 
 	/**
 	 * Creates a space related to the book.
 	 *
-	 * @return string
+	 * @return boolean
 	 */
-	public function createBookSpace($model, &$space_model)
+	public function createBookSpace($title, &$space_model)
 	{
-		$service_url = 'https://www.ziotopoulos.space/api/v1/space';
+		$ch = curl_init();
 		$data = array(
-			'name' => $model->title,
+			'name' => $title,
 			'visibility' => 1,
 		        'join_policy' => 1);
-		$url = $service_url;
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		require '/var/www/humhub/protected/modules/example-basic/controllers/.rest_api';
-		curl_setopt($ch, CURLOPT_USERPWD, "admin:$rest_api");
-		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-			
-		$response = json_decode(curl_exec($ch), true);
-		curl_close($ch);
+		$this->callRestApi($data, $ch);
 		
 		if(array_key_exists('id', $response)) {
 			$space_model->id = $response['id'];
@@ -79,11 +128,17 @@ class IndexController extends Controller
 			$space_model->name = $response['name'];
 			$space_model->description = $response['description'];
 			$space_model->url = $response['url'];
+
+			// Add the current user to the newly created space
+		        // TODO(aziot) Check if we can create the space directly on behalf of the user.
+			curl_reset($ch);
+	                $this->addUserToSpace($space_model->id, Yii::$app->user->identity->getId(), $ch);
+
 			return true;
 		}
 		return false;
 	}
-
+	
 	/**
 	 * Renders the index view for the module
 	 *
@@ -95,9 +150,9 @@ class IndexController extends Controller
 
 		if ($model->load(Yii::$app->request->post()) && $model->validate()) {
 			if ($this->queryKG($model)) {
-			        $space_model = new SpaceForm();
-				if ($this->createBookSpace($model, $space_model)) {
-			                return $this->render('space', ['model' => $space_model]);
+				$space_model = new SpaceForm();
+				if ($this->createBookSpace($model->title, $space_model)) {
+					return $this->render('space', ['model' => $space_model]);
 				} else {
 					return $this->render('book-confirm', ['model' => $model]);
 				}
